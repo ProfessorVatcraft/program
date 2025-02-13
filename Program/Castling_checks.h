f14fc5550e0853966d89cdd814936542ca73bfb4
ProfessorVatcraft <professorvatcraft@gmail.com>
2025-02-13 16:06:41 +0700

    Delete Program directory

--- Program/Castling_checks.h
+++
@@ -1,9 +0,0 @@
-#ifndef CASTLING_CHECKS_H_INCLUDED
-#define CASTLING_CHECKS_H_INCLUDED
-
-bool castling_kingside_white = true;
-bool castling_queenside_white = true;
-bool castling_kingside_black = true;
-bool castling_queenside_black = true;
-
-#endif //CASTLING_CHECKS
--- Program/En_passant_check.h
+++
@@ -1,6 +0,0 @@
-#ifndef EN_PASSANT_CHECK_H_INCLUDED
-#define EN_PASSANT_CHECK_H_INCLUDED
-
-int en_passant[2] = {-1, -1};
-
-#endif // EN_PASSANT_CHECK_H_INCLUDED
